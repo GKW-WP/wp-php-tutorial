b453c6e03ad4a522cd5e3254b31951fd30c464a6
Cruz Ramirez <cruz_ramirez@ymail.com>
2020-01-08 16:15:59 -0800

    hotfix on post links

--- wordpress/wp-content/themes/fictionaluniversity/index.php
+++ wordpress/wp-content/themes/fictionaluniversity/index.php
@@ -15,14 +15,15 @@
 	while(have_posts()){
 		the_post(); ?>
 		<div class="post-item">
-			<h2><a href="<?php the_permalink?>"><?php the_title();?></a></h2>
+			<h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
 
 			<div class="metabox">
-				<p>posted by brad on 6.8.17 in news</p>
+				<p>posted by <?php the_author_posts_link();?> on <?php the_time('F, j, Y');?> in <?php echo get_the_category_list(', ')?></p>
 			</div>
 
 			<div class="generic-content">
-				<?php the_exerpt();?>
+				<?php the_excerpt();?>
+				<p><a class="btn btn--blue" href="<?php the_permalink();?>">Continue reading &raquo;</a></p>
 			</div>
 		</div>
 
